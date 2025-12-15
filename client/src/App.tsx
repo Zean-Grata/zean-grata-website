8b7cc4afee3425d724eea6619b173a9dd5707173
Nahoom <nahoombelete24@gmail.com>
2025-12-15 02:05:25 +0200

    ...

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="bg-[url('background.jpg')] flex flex-col h-screen w-full bg-cover bg-center px-10 py-5 space-y-30">
+    <div className="bg-[url('./background.jpg')] flex flex-col h-screen w-full bg-cover bg-center px-10 py-5 space-y-30">
         <div className='flex'>
             <img src="logo.svg" className='h-40 w-40'></img>
         </div>
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 
 app.use(cors());
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname,'../client/dist')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,"../client/dist/index.html"));
