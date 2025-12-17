ea1724b0a133e794d9e5b296f6cd0288fea0808e
Nahoom <nahoombelete24@gmail.com>
2025-12-17 05:54:22 +0200

    Remove server folder and add logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className="bg-[url('/background.jpg')] flex flex-col h-screen w-full bg-cover bg-center px-10 py-5 space-y-30">
+    <div className="bg-[url('/bg_image.png')] flex flex-col h-screen w-full bg-cover bg-bottom-left px-10 py-15 space-y-30">
         <div className='flex'>
-            <img src="logo.svg" className='h-40 w-40'></img>
+            <img src="zean_grata_logo.png" className='h-30 w-30'></img>
         </div>
         <div className='flex justify-center items-center'>
             <h1 className='text-2xl text-white'>Website Under Construction</h1>
@@ -13,6 +13,5 @@ function App() {
        </div>
   );
 }
-
 
 export default App
