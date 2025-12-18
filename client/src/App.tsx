7a6a8de99e22e4bafbc72d6a9e72571e99a3fda7
Nahoom <nahoombelete24@gmail.com>
2025-12-18 22:43:33 +0200

    Add GitHub Actions S3 deploy pipeline

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className="bg-[url('/bg_image.png')] flex flex-col h-screen w-full bg-cover bg-bottom-left px-10 py-15 space-y-30">
+    <div className="bg-[url('/bg_image.png')] flex flex-col h-screen w-full bg-cover bg-top md:bg-bottom-left px-10 py-15 space-y-30">
         <div className='flex'>
-            <img src="zean_grata_logo.png" className='h-30 w-30'></img>
+            <img src="zean_grata_logo.png" className='h-20 w-20 md:h-30 md:w-30'></img>
         </div>
         <div className='flex justify-center items-center'>
             <h1 className='text-2xl text-white'>Website Under Construction</h1>
