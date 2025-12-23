04200963d424d2740b284b1b92d8628624c27e41
Nahoom <nahoombelete24@gmail.com>
2025-12-23 09:01:50 +0200

    Add landing page layout and content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,92 @@ import './App.css'
 
 function App() {
   return (
-    <div className="bg-[url('/bg_image.png')] flex flex-col h-screen w-full bg-cover bg-top md:bg-bottom-left px-10 py-15 space-y-30">
-        <div className='flex'>
-            <img src="zean_grata_logo.png" className='h-20 w-20 md:h-30 md:w-30'></img>
+    <div className='[&_*]:text-white'>
+       <div id="nav" className='flex px-35 py-5 h-[70px]'>
+            <img src="zean_grata_logo.svg" alt="Zean Grata company logo" role="img" aria-label='Zean Grata company logo' className='w-[200px] h-[30px]'/>
         </div>
-        <div className='flex justify-center items-center'>
-            <h1 className='text-2xl text-white'>Website Under Construction</h1>
+        <div id="bg-container" className="bg-[url('/bg_image.png')] flex flex-col h-fit w-full bg-cover px-10 py-15 space-y-30">
+          <div id="secondary-container" className='flex flex-col space-y-10 px-25'>
+            {/*BEGIN: Main Headline */}
+            <div className='flex flex-col space-y-1'>
+              <h1 className='text-white text-5xl text-balance max-w-[10ch]'>IT Consulting Services</h1>
+              <h2 className='text-[#F3F3F3] text-lg max-w-[30ch]'>We help your systems run smoothly so you can focus on your work.</h2>
+            </div>
+             {/*END: Main Headline */}
+             {/*BEGIN: Services and Team Container */}
+            <div className='flex space-x-10'>
+              {/*BEGIN: Services Container */}
+              <div className='flex flex-col space-y-10'>
+                <h1 className='text-2xl'>OUR SERVICES</h1>
+                <div className='grid grid-cols-2 gap-y-10 gap-x-10 [&>div>h2]:text-5xl [&>div>h2]:font-bold [&>div]:flex [&>div]:items-center [&>div]:space-x-2'>
+                  <div className=''>
+                    <h2>01.</h2>
+                    <h3 className='max-w-[15ch]'>Website Design & Setup</h3>
+                  </div>
+                   <div className=''>
+                    <h2>02.</h2>
+                    <h3>Computer System Installation & Upgrades</h3>
+                  </div>
+                   <div className=''>
+                    <h2>03.</h2>
+                    <h3>Cloud Computing & Cloud Setup</h3>
+                  </div>
+                   <div className=''>
+                    <h2>04.</h2>
+                    <h3>IT Support & Troubleshooting</h3>
+                  </div>
+                   <div className=''>
+                    <h2>05.</h2>
+                    <h3>Email & Business Tool Setup</h3>
+                  </div>
+                   <div className=''>
+                    <h2>06.</h2>
+                    <h3>Technology Advice for Small Businesses</h3>
+                  </div>
+                </div>
+              </div>
+              {/*END: Services Container */}
+              {/*START: Team Container*/}
+              <div className='flex flex-col space-y-10'>
+                <h1 className='text-2xl'>THE TEAM</h1>
+                <div className='flex space-x-10'>
+                  <div className='flex flex-col justify-around w-[280px] h-[250px] bg-[#08383399] p-2 space-y-5'>
+                    <div className='flex flex-1 justify-center'>
+                        <img src="/zean_grata_icon.svg" alt="Zean Grata Logo" className='w-[50px] h-[50px] color-white'/>
+                    </div>
+                    <div className='flex flex-1 items-end'>
+                      <p>Nahoom Belete<br/>
+                         IT Consultant<br/>
+                         +27 76 844 3280<br/> 
+                         nahoombelete@zeangrata.co.za
+                      </p>
+                    </div>
+                  </div>
+                  <div className='flex flex-col justify-around w-[280px] h-[250px] bg-[#08383399] p-2 space-y-5'>
+                    <div className='flex flex-1 justify-center'>
+                        <img src="/zean_grata_icon.svg" alt="Zean Grata Logo" className='w-[50px] h-[50px]'/>
+                    </div>
+                    <div className='flex flex-1 items-end'>
+                      <p>EmannuelBelete<br/>
+                         IT Consultant<br/>
+                         +27 79 455 8925<br/> 
+                         emannuelbelete@zeangrata.co.za
+                      </p>
+                    </div>   
+                  </div>
+                </div>
+              </div>
+              {/*END: Team Container*/}
+            </div>
+            {/*END: Services and Team Container */}
+          </div>  
         </div>
-        
-       </div>
+        <footer id="footer" className='flex justify-center items-center w-auto h-[100px] bg-[#2E2E2E]'>
+          <small>
+              &copy;{new Date().getFullYear()} Zean Grata (Pty) Ltd. All Rights Reserved.
+          </small>
+        </footer>
+    </div>
   );
 }
 
