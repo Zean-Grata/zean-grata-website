b5d8edd5b7b99c5e41d92948c7cc1f2a3af722c3
Nahoom <nahoombelete24@gmail.com>
2025-12-25 01:02:02 +0200

    Add icons to services list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
-import './App.css'
-import logo from '/src/assets/logo.svg'
+import './App.css';
+import logo from '/src/assets/logo.svg';
+import troubleshoot from '/src/assets/troubleshoot.svg';import work from '/src/assets/work.svg'; 
+import web from '/src/assets/web.svg';import email from '/src/assets/email.svg';
+import backup from '/src/assets/backup.svg';import advice from '/src/assets/advice.svg'
 
 function App() {
   return (
     <div className='[&_*]:text-white w-auto overflow-hidden'>
-       <div id="nav" className='flex px-15 lg:px-35 py-5 h-[70px] w-auto'>
+       <div id="nav" className='flex px-15 lg:px-30 py-5 h-[70px] w-auto'>
             <img src={logo} alt="Zean Grata company logo" role="img" aria-label='Zean Grata company logo' className='w-[150px] h-[20px] lg:w-[200px] lg:h-[30px]'/>
         </div>
-        <div id="bg-container" className="bg-hero flex flex-col h-fit w-auto bg-cover bg-center lg:bg-top px-10 py-15 space-y-30">
+        <div id="bg-container" className="bg-hero flex flex-col h-fit w-auto bg-cover bg-center lg:bg-top px-5 py-15 space-y-30">
           <div id="secondary-container" className='flex flex-col items-center sm:items-start space-y-10 px-5 lg:px-25'>
             {/*BEGIN: Main Headline */}
             <div className='flex flex-col space-y-1'>
@@ -16,40 +19,42 @@ function App() {
             </div>
              {/*END: Main Headline */}
              {/*BEGIN: Services and Team Container */}
-            <div className='flex flex-col space-y-10 lg:space-x-10 xl:flex-row'>
+            <div className='flex flex-col space-y-10 w-full lg:space-x-5 xl:flex-row'>
               {/*BEGIN: Services Container */}
-              <div className='flex flex-col space-y-10 lg:items-start'>
+              <div className='flex flex-2 flex-col space-y-10 lg:items-start'>
                 <h1 className='text-2xl'>OUR SERVICES</h1>
-                <div className='grid grid-cols-1 sm:grid-cols-2 gap-y-10 gap-x-10 [&>div>h2]:text-3xl lg:[&>div>h2]:text-5xl [&>div>h2]:font-bold [&>div]:flex [&>div]:items-center [&>div]:space-x-2 [&>div>h3]:text-sm [&>div>h3]:max-w-[30ch]'>
-                  <div className=''>
-                    <h2>01.</h2>
-                    <h3 className=''>Web & Digital Solutions</h3>
+                <div className='grid grid-cols-1 justify-end sm:grid-cols-2 gap-y-10 gap-x-10 [&>div>h2]:text-3xl 
+                lg:[&>div>h2]:text-5xl [&>div>h2]:font-bold [&>div]:flex [&>div]:items-center [&>div]:space-x-2
+                [&>div>h3]:text-lg [&>div>img]:w-[50px] [&>div>img]:h-[50px]'>
+                  <div>
+                    <img src={troubleshoot}/>
+                    <h3 className=''>IT Support & Troubleshooting</h3>
                   </div>
                    <div className=''>
-                    <h2>02.</h2>
-                    <h3>IT Systems & Support</h3>
+                    <img src={work}/>
+                    <h3>Computer & Office Setup</h3>
                   </div>
                    <div className=''>
-                    <h2>03.</h2>
-                    <h3>Cloud Computing & Cloud Setup</h3>
+                    <img src={web}/>
+                    <h3>Website Setup & Maintenance</h3>
                   </div>
                    <div className=''>
-                    <h2>04.</h2>
-                    <h3>IT Support & Troubleshooting</h3>
+                    <img src={email}/>
+                    <h3>Email & Cloud Tools Setup</h3>
                   </div>
                    <div className=''>
-                    <h2>05.</h2>
-                    <h3>Email & Business Tool Setup</h3>
+                    <img src={backup}/>
+                    <h3>Data Backup & Basic Security</h3>
                   </div>
                    <div className=''>
-                    <h2>06.</h2>
-                    <h3>Technology Advice for Small Businesses</h3>
+                    <img src={advice}/>
+                    <h3>Technology Advice & Guidance</h3>
                   </div>
                 </div>
               </div>
               {/*END: Services Container */}
               {/*START: Team Container*/}
-              <div className='flex flex-col space-y-10'>
+              <div className='flex flex-1 flex-col space-y-10'>
                 <h1 className='text-2xl'>THE TEAM</h1>
                 <div className='flex flex-col space-y-10 sm:flex-row lg:space-x-10'>
                   <div className='flex flex-col justify-around w-[250px] h-[200px] lg:w-[280px] lg:h-[250px] bg-[#08383399] p-2 space-y-5'>
