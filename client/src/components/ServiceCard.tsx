96e1b1210afc52e637f5b2f2ddaa2236304b8f7b
Nahoom <nahoombelete24@gmail.com>
2025-12-25 01:36:35 +0200

    Add comment to unused imports and edit deploy file

--- client/src/components/ServiceCard.tsx
+++ client/src/components/ServiceCard.tsx
@@ -1,6 +1,6 @@
-import troubleshoot from '/src/assets/troubleshoot.svg';import work from '/src/assets/work.svg'; 
-import web from '/src/assets/web.svg';import email from '/src/assets/email.svg';
-import backup from '/src/assets/backup.svg';import advice from '/src/assets/advice.svg'
+//import troubleshoot from '/src/assets/troubleshoot.svg';import work from '/src/assets/work.svg'; 
+//import web from '/src/assets/web.svg';import email from '/src/assets/email.svg';
+//import backup from '/src/assets/backup.svg';import advice from '/src/assets/advice.svg'
 
 interface ServiceCardProps {
     iconName: string;
